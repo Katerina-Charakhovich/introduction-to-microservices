6ad0749fbcc7b3d8cab743b7aedae3c7d18d7607
Katerina_Charakhovich <v-kcharakhovich@expediagroup.com>
2023-10-15 09:27:31 +0200

    task3: initial commit

--- resource-service/src/main/java/com/epam/microservice/resource/ResourceServiceApplication.java
+++ resource-service/src/main/java/com/epam/microservice/resource/ResourceServiceApplication.java
@@ -2,10 +2,12 @@ package com.epam.microservice.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class ResourceServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResourceServiceApplication.class, args);
